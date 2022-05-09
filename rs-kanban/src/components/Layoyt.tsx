3641798cb46fe3129c497bf48b1133c8dc5346ed
lost-fox <Oljaburak@mail.ru>
2022-05-09 11:15:20 +0300

    feat: add form create new board

--- rs-kanban/src/components/Layoyt.tsx
+++ rs-kanban/src/components/Layoyt.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import { Footer } from './Footer';
+import { Header } from './Header';
 
 export const Layout = () => {
   return (
     <>
+      <Header />
       <Outlet />
       <Footer />
     </>
