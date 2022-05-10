fd05895e29a245c793260fa865ea32fd9b48b6be
Andrey Chibisov <achibisov@skyori.ru>
2022-05-10 13:27:47 +0500

    refactor: add lang types and change toggle lang function name

--- rs-kanban/src/components/langSwitcher/index.tsx
+++ rs-kanban/src/components/langSwitcher/index.tsx
@@ -3,22 +3,25 @@ import Typography from '@mui/material/Typography';
 import { useTranslation } from 'react-i18next';
 import { useState } from 'react';
 import { AntSwitch } from './AntSwitch';
+import { Lang } from './types/lang';
+import { Switch } from '@mui/material';
 
 export const LangSwitcher: React.FC = () => {
   const { i18n } = useTranslation();
-  const [lang, setLang] = useState('en');
+  const [lang, toggleLang] = useState<string>(Lang.en);
 
   const changeLang = () => {
-    const currentLang = lang === 'en' ? 'ru' : 'en';
-    setLang(currentLang);
+    const currentLang = lang === Lang.en ? Lang.ru : Lang.en;
+    toggleLang(currentLang);
     i18n.changeLanguage(currentLang);
   };
 
   return (
-    <Stack direction="row" spacing={1} alignItems="center">
-      <Typography>ru</Typography>
-      <AntSwitch defaultChecked inputProps={{ 'aria-label': 'ant design' }} onClick={changeLang} />
-      <Typography>en</Typography>
-    </Stack>
+    <Switch defaultChecked />
+    // <Stack direction="row" spacing={1} alignItems="center">
+    //   <Typography>ru</Typography>
+    //   <AntSwitch defaultChecked inputProps={{ 'aria-label': 'ant design' }} onClick={changeLang} />
+    //   <Typography>en</Typography>
+    // </Stack>
   );
 };
--- rs-kanban/src/pages/BoardPage/index.tsx
+++ rs-kanban/src/pages/BoardPage/index.tsx
@@ -1,7 +1,7 @@
 import './style.scss';
 import { useTranslation } from 'react-i18next';
 
-export const Board = () => {
+export const BoardPage = () => {
   const { t } = useTranslation();
   return <div className="board">{t('pages.board')}</div>;
 };
