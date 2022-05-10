f5423dedc5ac7b0699c2cd72015d4e3604972c32
manol1 <marsen1491@gmail.com>
2022-05-11 02:56:00 +0300

    fix: bugs from comments

---
+++ rs-kanban/src/components/StickyHeader/index.tsx
@@ -0,0 +1,34 @@
+import './style.scss';
+import { NavLink } from 'react-router-dom';
+import { Routes } from '../../types/routes';
+import { useEffect, useState } from 'react';
+import { Button } from '@mui/material';
+
+export const StickyHeader: React.FC = () => {
+  const [isSticky, setIsSticky] = useState<boolean>(false);
+
+  const isStickyFn = () => {
+    window.scrollY > 0 ? setIsSticky(true) : setIsSticky(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', isStickyFn);
+    return () => {
+      window.removeEventListener('scroll', isStickyFn);
+    };
+  });
+
+  const className = `container ${isSticky ? 'stickyHeader' : ''}`;
+
+  return (
+    <div className={className}>
+      <div className="btnsContainer">
+        <NavLink to={Routes.editProfile} className="textDecorationNone">
+          <Button variant="contained">profile</Button>
+        </NavLink>
+        <Button variant="contained">logout</Button>
+        <Button variant="contained">+ board</Button>
+      </div>
+    </div>
+  );
+};
--- rs-kanban/src/pages/EditProfile/index.tsx
+++ rs-kanban/src/pages/EditProfile/index.tsx
@@ -1,24 +1,22 @@
-import styles from './style.module.scss';
+import './style.scss';
+import { Box, TextField, Button } from '@mui/material';
 
 export const EditProfile: React.FC = () => {
   return (
-    <div className={styles.container}>
-      <form className={styles.form} onSubmit={(e) => e.preventDefault()}>
-        <label>
-          Name:
-          <input name="name" />
-        </label>
-        <label>
-          Login:
-          <input name="login" />
-        </label>
-        <label>
-          Password:
-          <input name="password" />
-        </label>
-        <button type="submit">Save Changes</button>
-      </form>
-      <button className={styles.deleteAccount}>Delete account</button>
+    <div className="edit-profile-container">
+      <Box
+        component="form"
+        className="edit-profile-form"
+        onSubmit={(e: React.SyntheticEvent) => e.preventDefault()}
+      >
+        <TextField placeholder="Enter your name" required />
+        <TextField placeholder="Enter your login" required />
+        <TextField type="password" placeholder="Enter your password" required />
+        <Button variant="contained">Save Changes</Button>
+      </Box>
+      <Button variant="contained" color="error" style={{ width: '200px', marginBottom: '40px' }}>
+        Delete account
+      </Button>
     </div>
   );
 };
