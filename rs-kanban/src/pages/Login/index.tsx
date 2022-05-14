0984f4ff110024629bccb8afc460857a2533b0e7
Olga Provedentceva <77030402+lost-fox@users.noreply.github.com>
2022-05-14 13:24:15 +0300

    Rr 28 login\registration (#12)
    * feat: add request registration
    
    * feat: add request login
    
    * fix: bugs fix

--- rs-kanban/src/pages/Login/index.tsx
+++ rs-kanban/src/pages/Login/index.tsx
@@ -2,25 +2,59 @@ import './style.scss';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import { useState } from 'react';
+import { createToken } from '../../api/authorization/create-token.api';
+import { useNavigate } from 'react-router-dom';
+import { Routes } from '../../models/routes';
+import { ONE_DAY } from './constant';
+import { LoginFormEvent } from './interface/LoginForm';
 
 export const Login: React.FC = () => {
+  const navigate = useNavigate();
+  const [forms, setForm] = useState({
+    login: '',
+    password: '',
+  });
+
+  const changeHandler = (event: LoginFormEvent) => {
+    setForm({ ...forms, [event.target.name]: event.target.value });
+  };
+
+  const reqisterHandler = () => {
+    const date = new Date(Date.now() + ONE_DAY).toUTCString();
+    const answer = createToken(forms);
+    answer.then((resolve) => {
+      document.cookie = `Token=${resolve.token}; expires=${date}`;
+      navigate(Routes.main);
+    });
+  };
+
   return (
     <div className="container-login">
       <div className="login__title">Login</div>
       <Box component="form" className="login__form">
         <div className="login__form-input">
-          <TextField required id="outlined-required" label="Login" placeholder="Enter your login" />
+          <TextField
+            required
+            name="login"
+            id="outlined-required"
+            label="Login"
+            placeholder="Enter your login"
+            onChange={changeHandler}
+          />
         </div>
         <div className="login__form-input">
           <TextField
             required
+            name="password"
             id="outlined-required"
             label="Password"
             placeholder="Enter your password"
             type="password"
+            onChange={changeHandler}
           />
         </div>
-        <Button className="login__form-btn" variant="contained">
+        <Button className="login__form-btn" variant="contained" onClick={reqisterHandler}>
           Click
         </Button>
       </Box>
--- rs-kanban/src/pages/Registration/index.tsx
+++ rs-kanban/src/pages/Registration/index.tsx
@@ -2,47 +2,66 @@ import './style.scss';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
-import { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { createAccount } from '../../api/authorization/create-account.api';
+import { Link } from 'react-router-dom';
+import { Routes } from '../../models/routes';
 
 export const Registration: React.FC = () => {
   const [validName, setValidName] = useState<boolean>(true);
   const [validLogin, setValidLogin] = useState<boolean>(true);
   const [validPassword, setValidPassword] = useState<boolean>(true);
   const [validButton, setValidButton] = useState<boolean>(true);
+  const [forms, setForm] = useState({
+    name: '',
+    login: '',
+    password: '',
+  });
 
-  const setName = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.value.length < 2) {
-      setValidName(true);
-    } else {
-      setValidName(false);
+  useEffect(() => {
+    const isValid = !validName && !validLogin && !validPassword;
+    setValidButton(!isValid);
+  }, [validName, validLogin, validPassword]);
+
+  const changeHandler = (event: React.ChangeEvent) => {
+    const name = (event.target as HTMLInputElement).name;
+    const value = (event.target as HTMLInputElement).value;
+    setForm({ ...forms, [name]: value });
+    switch (name) {
+      case 'name':
+        setName(event);
+        break;
+      case 'login':
+        setLogin(event);
+        break;
+      case 'password':
+        setPassword(event);
+        break;
+      default:
+        break;
     }
-    disabledButton();
   };
 
-  const setLogin = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.value.length < 3) {
-      setValidLogin(true);
-    } else {
-      setValidLogin(false);
-    }
-    disabledButton();
+  const setName = (e: React.ChangeEvent) => {
+    const value = (e.target as HTMLInputElement).value;
+    const isValidName = value.length < 2;
+    setValidName(isValidName);
   };
 
-  const setPassword = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.value.length < 6) {
-      setValidPassword(true);
-    } else {
-      setValidPassword(false);
-    }
-    disabledButton();
+  const setLogin = (e: React.ChangeEvent) => {
+    const value = (e.target as HTMLInputElement).value;
+    const isValidLogin = value.length < 3;
+    setValidLogin(isValidLogin);
   };
 
-  const disabledButton = () => {
-    if (!validName && !validLogin && !validPassword) {
-      setValidButton(false);
-    } else {
-      setValidButton(true);
-    }
+  const setPassword = (e: React.ChangeEvent) => {
+    const value = (e.target as HTMLInputElement).value;
+    const isValidPassword = value.length <= 6;
+    setValidPassword(isValidPassword);
+  };
+
+  const reqisterHandler = () => {
+    createAccount(forms);
   };
 
   return (
@@ -52,40 +71,50 @@ export const Registration: React.FC = () => {
         <div className="registration__form-input">
           <TextField
             required
+            name="name"
             id="outlined-required"
             label="Name"
             placeholder="Enter your name"
             error={validName}
-            onChange={setName}
+            onChange={changeHandler}
             helperText="Enter min 2 symbols"
           />
         </div>
         <div className="registration__form-input">
           <TextField
             required
+            name="login"
             id="outlined-required"
             label="Login"
             placeholder="Enter your login"
             error={validLogin}
-            onChange={setLogin}
+            onChange={changeHandler}
             helperText="Enter min 3 symbols"
           />
         </div>
         <div className="registration__form-input">
           <TextField
             required
+            name="password"
             id="outlined-required"
             label="Password"
             placeholder="Enter your password"
             type="password"
             error={validPassword}
-            onChange={setPassword}
-            helperText="Enter min 6 symbols"
+            onChange={changeHandler}
+            helperText="Enter more than 6 characters"
           />
         </div>
-        <Button className="registration__form-btn" variant="contained" disabled={validButton}>
-          Click
-        </Button>
+        <Link to={Routes.login} className="registration__form-link">
+          <Button
+            className="registration__form-btn"
+            variant="contained"
+            disabled={validButton}
+            onClick={reqisterHandler}
+          >
+            Click
+          </Button>
+        </Link>
       </Box>
     </div>
   );
