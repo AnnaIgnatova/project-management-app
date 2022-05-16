08f0a7138dc71ea27bb61251e14b93f4eb23650e
lost-fox <Oljaburak@mail.ru>
2022-05-16 14:16:13 +0300

    feat: change interface

--- rs-kanban/src/components/cardTask/interface/ModalWindowProps.ts
+++ rs-kanban/src/components/cardTask/interface/ModalWindowProps.ts
@@ -1,7 +1,21 @@
-import { TaskType } from 'features/task/taskSlice';
-
 export interface ModalWindowProps {
   open: boolean;
   onClose: () => void;
   value: TaskType;
+}
+
+interface TaskType {
+  id: string;
+  title: string;
+  order: number;
+  description: string;
+  userId: string;
+  boardId: string;
+  columnId: string;
+  files: FilesTaksType[];
+}
+
+interface FilesTaksType {
+  taskId: string;
+  file: string;
 }
--- rs-kanban/src/features/task/taskSlice.ts
+++ rs-kanban/src/features/task/taskSlice.ts
@@ -1,25 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-interface InitialState {
-  task: TaskType;
-}
-export interface TaskType {
-  id: string;
-  title: string;
-  order: number;
-  description: string;
-  userId: string;
-  boardId: string;
-  columnId: string;
-  files: FilesTaksType[];
-}
-
-interface FilesTaksType {
-  taskId: string;
-  file: string;
-}
-
-const initialState: InitialState = {
+const initialState = {
   task: {
     id: '',
     title: '',
