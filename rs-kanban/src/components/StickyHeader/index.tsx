9892642762119b78db92234fafa6409204764e5b
lost-fox <Oljaburak@mail.ru>
2022-05-22 13:44:48 +0300

    feat: add buttong on header

--- rs-kanban/src/components/StickyHeader/index.tsx
+++ rs-kanban/src/components/StickyHeader/index.tsx
@@ -1,24 +1,47 @@
 import './style.scss';
-import { Link } from 'react-router-dom';
+import { Link, Navigate } from 'react-router-dom';
 import { Routes } from '../../models/routes';
 import { Button } from '@mui/material';
 import { useAppSelector } from '../../store';
 import { GetValueToken } from '../../utils/getValueToken';
 import { useTranslation } from 'react-i18next';
+import { LangSwitcher } from '../langSwitcher';
 
 export const StickyHeader: React.FC = () => {
   const { t } = useTranslation();
-  const token = useAppSelector((state) => state.tokenReduser.isToken);
+  const { isToken, token } = useAppSelector((state) => state.tokenReduser);
   GetValueToken();
 
+  const signOut = () => {
+    document.cookie = `Token=${token}; expires=Thu, 01 Jan 1970 00:00:00 GMT;`;
+    <Navigate to={Routes.welcome} />;
+    // location.reload();
+  };
+
   const getBtnHeader = () => {
-    if (token) {
+    if (isToken) {
+      if (location.pathname === '/') {
+        return (
+          <Link to={Routes.main}>
+            <Button className="main-btn" variant="contained">
+              {t('header.btnMain')}
+            </Button>
+          </Link>
+        );
+      }
+
       return (
-        <Link to={Routes.main}>
-          <Button className="main-btn" variant="contained">
-            {t('header.btnMain')}
+        <>
+          <LangSwitcher />
+          <Button variant="contained">Create new board</Button>
+
+          <Link to={Routes.editProfile}>
+            <Button variant="contained">Edit Profile</Button>
+          </Link>
+          <Button variant="contained" color="error" onClick={signOut}>
+            Sign Out
           </Button>
-        </Link>
+        </>
       );
     }
 
