e226ab9dd8d2351308569a5176c5c4184459f2ec
Olga Provedentceva <77030402+lost-fox@users.noreply.github.com>
2022-05-22 17:25:43 +0300

    Add redirect  (#26)
    * feat: add redirect
    
    * fix: bugs fix

--- rs-kanban/src/App.tsx
+++ rs-kanban/src/App.tsx
@@ -1,29 +1,40 @@
-import { Route, Routes as RoutesWrapper } from 'react-router-dom';
+import { Navigate, Route, Routes as RoutesWrapper } from 'react-router-dom';
 import './App.css';
 import { Login } from './pages/Login';
 import { BoardPage } from './pages/BoardPage';
 import { Main } from './pages/Main';
-import { NotFound } from './pages/NotFound';
 import { User } from './pages/User';
 import { Welcome } from './pages/Welcome';
 import { Registration } from './pages/Registration';
 import { Routes } from './models/routes';
 import { Layout } from './components/Layoyt';
 import { EditProfile } from './pages/EditProfile';
+import { useAppSelector } from './store';
 
 const AppRouter = () => {
-  return (
+  const isToken = useAppSelector((state) => state.tokenReduser.isToken);
+
+  return isToken ? (
     <>
       <RoutesWrapper>
         <Route path={Routes.welcome} element={<Layout />}>
           <Route index element={<Welcome />} />
           <Route path={Routes.main} element={<Main />} />
-          <Route path={Routes.login} element={<Login />} />
-          <Route path={Routes.registration} element={<Registration />} />
           <Route path={Routes.user} element={<User />} />
           <Route path={Routes.board} element={<BoardPage />} />
           <Route path={Routes.editProfile} element={<EditProfile />} />
-          <Route path={Routes.notFound} element={<NotFound />} />
+          <Route path="*" element={<Navigate to={Routes.main} />} />
+        </Route>
+      </RoutesWrapper>
+    </>
+  ) : (
+    <>
+      <RoutesWrapper>
+        <Route path={Routes.welcome} element={<Layout />}>
+          <Route index element={<Welcome />} />
+          <Route path={Routes.login} element={<Login />} />
+          <Route path={Routes.registration} element={<Registration />} />
+          <Route path="*" element={<Navigate to={Routes.welcome} />} />
         </Route>
       </RoutesWrapper>
     </>
