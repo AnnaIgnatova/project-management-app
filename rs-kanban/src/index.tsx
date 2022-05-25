fdcb708fe3871f361885a63e01fd67845d6f4ebe
manol1 <marsen1491@gmail.com>
2022-05-25 21:51:39 +0300

    feat: change toast message

--- rs-kanban/src/index.tsx
+++ rs-kanban/src/index.tsx
@@ -3,36 +3,26 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import AppRouter from './App';
-import { AxiosError } from 'axios';
 import { instanceAxios } from './services';
 import './i18n';
 import { ToastContainer, toast, Flip } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Provider } from 'react-redux';
 import { store } from './store';
-import { HttpStatus, ToastMessage } from './services/api-service/http-status';
+import axios from 'axios';
 
 instanceAxios.interceptors.response.use(
   (response) => response,
-  (error: AxiosError) => {
-    switch (error.response?.status) {
-      case HttpStatus.BAD_REQUEST:
-        toast.error(ToastMessage.BAD_REQUEST);
-        break;
-      case HttpStatus.UNAUTHORIZED:
-        toast.error(ToastMessage.UNAUTHORIZED);
-        break;
-      case HttpStatus.NOT_FOUND:
-        toast.error(ToastMessage.NOT_FOUND);
-        break;
-      case HttpStatus.CONFLICT:
-        toast.error(ToastMessage.CONFLICT);
-        break;
-      case HttpStatus.INTERNAL_SERVER_ERROR:
-        toast.error(ToastMessage.INTERNAL_SERVER_ERROR);
-        break;
-      default:
-        break;
+  (error) => {
+    const statusText: string | undefined = error.response?.data?.message;
+    if (axios.isAxiosError(error)) {
+      if (statusText) {
+        toast.error(`Oops... ${statusText}`);
+      } else {
+        toast.error(`Unexpected error (${error.response?.status}). Please try again later.`);
+      }
+    } else {
+      toast.error('Unknown error. We are shocked too...');
     }
   }
 );
--- rs-kanban/src/services/api-service/http-status.ts
+++ rs-kanban/src/services/api-service/http-status.ts
@@ -8,11 +8,3 @@ export enum HttpStatus {
   CONFLICT = 409,
   INTERNAL_SERVER_ERROR = 500,
 }
-
-export enum ToastMessage {
-  BAD_REQUEST = 'The request was not recognized by the server due to a possible syntax error',
-  UNAUTHORIZED = 'You are not authorized. Please sign in',
-  NOT_FOUND = 'The requested data was not found on this server',
-  CONFLICT = 'The request cannot be completed due to a conflict with the current state of the resource',
-  INTERNAL_SERVER_ERROR = 'Internal Server Error',
-}
