61c056c5f032cefc7003250de471d94e18fb51e2
Anna <missjellx@gmail.com>
2022-05-29 14:44:01 +0300

    feat: add back link

--- rs-kanban/src/pages/NotFound/index.tsx
+++ rs-kanban/src/pages/NotFound/index.tsx
@@ -1,7 +1,14 @@
 import './style.scss';
 import { useTranslation } from 'react-i18next';
+import { BackLink } from '../../components/backLink';
+import { Container } from '@mui/material';
 
 export const NotFound = () => {
   const { t } = useTranslation();
-  return <div className="not-found">{t('pages.notFound')}</div>;
+  return (
+    <Container maxWidth="xl" className="boardPage">
+      <BackLink />
+      {t('pages.notFound')}
+    </Container>
+  );
 };
